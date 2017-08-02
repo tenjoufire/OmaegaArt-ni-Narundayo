a44b1e80b25e71ffa5278f92281c400b4a485ed8
Kaisei Tsujimoto <puyopuyo.fv@gmail.com>
2017-08-02 18:54:31 +0900

    Initial commit

---
+++ Assets/Scripts/KinectCoordinateMapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+
+public class KinectCoordinateMapper : MonoBehaviour
+{
+
+    private KinectSensor kinectSensor;
+    private MultiSourceFrameReader multiSouseFrameReader;
+    private CoordinateMapper coordinateMapper;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        kinectSensor = KinectSensor.GetDefault();
+        multiSouseFrameReader = kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.BodyIndex | FrameSourceTypes.Color | FrameSourceTypes.Depth);
+        coordinateMapper = kinectSensor.CoordinateMapper;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
